0be0a6bbb0546fbeecdf0ed0e45f9277c2fb7e51
jsaique <jsaique11@gmail.com>
2023-02-12 20:03:43 -0500

    Added nav and login functionality

---
+++ components/Nav.jsx
@@ -0,0 +1,42 @@
+import Link from "next/link";
+import { auth } from "@/utils/firebase";
+import { useAuthState } from "react-firebase-hooks/auth";
+
+export default function Nav() {
+  const [user, loading] = useAuthState(auth);
+  console.log(user);
+  return (
+    <div>
+      <nav className="flex justify-between items-center py-10">
+        <Link href="/">
+          <button className="text-lg font-medium">Thoughty</button>
+        </Link>
+        <ul className="flex items-center gap-10">
+          {!user && (
+            <Link href={"/auth/login"}>
+              <button className="py-2 px-4 text-sm bg-cyan-500 text-white rounded-lg font-medium ml-8">
+                Login
+              </button>
+            </Link>
+          )}
+          {user && (
+            <div className="flex items-center gap-6">
+              <Link href="/post">
+                <button className="font-medium bg-cyan-500 text-white py-2 px-4 rounded-md text-sm">
+                  Post
+                </button>
+              </Link>
+              <Link href="/dashboard">
+                <img
+                  className="w-10 rounded-full cursor-pointer"
+                  src={user.photoURL}
+                  alt={user.displayName}
+                />
+              </Link>
+            </div>
+          )}
+        </ul>
+      </nav>
+    </div>
+  );
+}
---
+++ pages/auth/login.jsx
@@ -0,0 +1,68 @@
+import { FcGoogle } from "react-icons/fc";
+import { BsDisplayFill } from "react-icons/bs";
+import {
+  signInWithPopup,
+  GoogleAuthProvider,
+  signInAnonymously,
+} from "firebase/auth";
+import { auth } from "@/utils/firebase";
+import { useRouter } from "next/router";
+import { useAuthState } from "react-firebase-hooks/auth";
+import { useEffect } from "react";
+
+export default function Login() {
+  const route = useRouter();
+  const [user, loading] = useAuthState(auth);
+  //Google Sign in
+  const goggleProvider = new GoogleAuthProvider();
+  const GoogleLogin = async () => {
+    try {
+      const res = await signInWithPopup(auth, goggleProvider);
+      route.push("/");
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  // Demo Sign in
+  const AnonymousLogin = async () => {
+    try {
+      await signInAnonymously(auth);
+      route.push("/");
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  useEffect(() => {
+    if (user) {
+      route.push("/");
+    } else {
+      console.log("login");
+    }
+  }, [user]);
+
+  return (
+    <div className="shadow-xl mt-32 p-10 text-gray-700 rounded-lg">
+      <h2 className="text-2xl font-medium">Login</h2>
+      <div className="py-4">
+        <h3 className="py-4">Sign in with one of the providers</h3>
+        <button
+          onClick={GoogleLogin}
+          className="text-white bg-gray-700 w-full font-meduim rounded-lg flex align-middle p-4 gap-2"
+        >
+          <FcGoogle className="text-2xl " /> Sign in with Google
+        </button>
+      </div>
+      <div className="py-4">
+        <h4 className="py-4">Demo</h4>
+        <button
+          onClick={AnonymousLogin}
+          className="text-white bg-gray-700 w-full font-meduim rounded-lg flex align-middle p-4 gap-2"
+        >
+          <BsDisplayFill className="text-2xl " /> Demo User
+        </button>
+      </div>
+    </div>
+  );
+}
